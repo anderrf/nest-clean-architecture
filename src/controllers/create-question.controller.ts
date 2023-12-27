9a38df1a218b2378a81dac573a1b3d82a42ddb74
anderrf <anderson_rocha_farias@outlook.com>
2023-12-27 15:18:57 -0300

    chore: addition of jwt authentication

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user.decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor() {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
+    return ''
+  }
+}
